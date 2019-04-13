3ed0d7c03f246b3e19e600c06af6e1b69f8f3d09
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-04-12 19:00:21 -0700

    stable, created beeves object

--- background.js
+++ background.js
@@ -4,25 +4,6 @@ Beeves.beevesFileEndpoint = browser.extension.getURL("beeves.json");
 const postmanEndpoint = 'https://postman-echo.com/post';
 //const backendMockEndpoint = 'http://localhost:8080/';
 
-function log(message){
-  console.log("-----------------------");
-  console.log("beeves-extension says:");
-  console.log(message);
-  console.log("-----------------------");
-}
-
-//retrieve from an endpoint and return text
-async function getTextData(endpoint) {
-  try {
-    let res = await fetch(endpoint);
-    res = await res.text();
-    //log(res);
-    return res;
-    }catch(err) {
-      log(err);
-    }
-}
-
 //retrieve from an endpoint and return json 
 async function getJSONData(endpoint) {
   try {
@@ -31,42 +12,12 @@ async function getJSONData(endpoint) {
   //log(res);
   return res;
   }catch(err) {
-    log(err);
+    console.log(err);
   }
 }
 
-//post data to an endpoint
-async function postData(endpoint, payload) {
-  try {
-    let res = await fetch(endpoint, {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    });
-    res = await res.json();
-    //log(res);
-    return res;
-  }catch(err) {
-    log(err);
-  }
-}
 
-//send base extension the beevesfile when installed
-browser.runtime.onInstalled.addListener(async function(details){
-  let beevesJSON = await getJSONData(Beeves.beevesFileEndpoint);
-  let sending = await browser.runtime.sendMessage(
-    'base@beeves.com',
-    beevesJSON,
-    {}
-  );
-});
-
-browser.runtime.onMessageExternal.addListener(messageHandler);
-
-async function messageHandler(message, sender, sendResponse){
+Beeves.messageHandler = async function(message, sender, sendResponse){
     if(message['type'] == 'beevesRPC'){
         response = await beevesInvoker(message);
         return Promise.resolve(response);
@@ -80,22 +31,35 @@ async function beevesInvoker(message){
 }
 
 Beeves.beevesFunctions = {
-    newFunction: function(name, func){
-      this[name] = func;
-    },
     test: function(arg){
-      log(`beevesRPC works! data: ${arg}`);
+      console.log(`beevesRPC works! data: ${arg}`);
       return true;
     }
 };
 
-Beeves.beevesFunctions.newFunction('add', function(n1, n2){
-  log(n1+n2);
-  return n1+n2;
-});
+Beeves.newFunction = function(name, func){
+  this.beevesFunctions[name] = func;
+}
 
-Beeves.beevesFunctions.newFunction('subtract', function(n1, n2){
-  log(n1-n2);
-  return n1-n2;
-});
+Beeves.init = function(){
+  browser.runtime.onInstalled.addListener(async function(details){
+    let beevesJSON = await getJSONData(Beeves.beevesFileEndpoint);
+    let sending = await browser.runtime.sendMessage(
+      'base@beeves.com',
+      beevesJSON,
+      {}
+    );
+  });
+  browser.runtime.onMessageExternal.addListener(this.messageHandler);
+  Beeves.newFunction('add', function(n1, n2){
+    console.log(n1+n2);
+    return n1+n2;
+  });
+  
+  Beeves.newFunction('subtract', function(n1, n2){
+    console.log(n1-n2);
+    return n1-n2;
+  });
+}
 
+Beeves.init();
